f5b0a4c56e8bc00d30bc2845d95d7369951ac8e0
Cris M <cristian.montenegro.p@gmail.com>
2020-12-17 16:33:49 -0800

    feat(code style): service-worker.js linted following AirBnb standard

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,54 +1,48 @@
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/css/styles.css",
-  "/manifest.json",
-  "/js/index.js",
-  "/js/indexedDb.js",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/manifest.json',
+  '/js/index.js',
+  '/js/indexedDb.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
 ];
 
-const PRECACHE = "precache-v1";
-const RUNTIME = "runtime";
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
 
-self.addEventListener("install", event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(PRECACHE)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
-      .then(self.skipWaiting())
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting()),
   );
 });
 
 // The activate handler takes care of cleaning up old caches.
-self.addEventListener("activate", event => {
+self.addEventListener('activate', (event) => {
   const currentCaches = [PRECACHE, RUNTIME];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
+    caches.keys()
+      .then((cacheNames) => cacheNames.filter((cacheName) => !currentCaches.includes(cacheName)))
+      .then((cachesToDelete) => Promise.all(cachesToDelete.map((cacheToDelete) => caches
+        .delete(cacheToDelete))))
+      .then(() => self.clients.claim()),
   );
 });
 
-self.addEventListener("fetch", event => {
-  if (event.request.url.includes("/api/")) {
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('/api/')) {
     // make network request and fallback to cache if network request fails (offline)
     event.respondWith(
-      caches.open(RUNTIME).then(cache => {
-        return fetch(event.request)
-          .then(response => {
-            if (response.status === 200) {
-              cache.put(event.request.url, response.clone());
-            }
-            return response;
-          })
-          .catch(() => caches.match(event.request));
-      })
+      caches.open(RUNTIME).then((cache) => fetch(event.request)
+        .then((response) => {
+          if (response.status === 200) {
+            cache.put(event.request.url, response.clone());
+          }
+          return response;
+        }).catch(() => caches.match(event.request))),
     );
-    return;
   }
 });
